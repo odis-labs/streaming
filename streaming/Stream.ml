0f0443838dc2d64b284c43d9df5056397ddb7cc9
nuno <nuno.cravino@hyper.ag>
2021-11-18 17:33:22 +0000

    Bug correction

--- streaming/Stream.ml
+++ streaming/Stream.ml
@@ -215,12 +215,11 @@ let[@inline] cartesian_range ?by_i:(step_i = 1) ?by_j:(step_j = 1) f n0 m0 n1 m1
     invalid_arg "Streaming.Stream.cartesian_range: invalid range"
   else
     let[@inline] stream (Sink k) =
-      let rec loop i j r =
-        let v = f i j in
+      let rec loop i j r =        
         if k.full r then r
-        else if i >= m0 && j < m1 then loop n0 (j + step_j) (k.push r v)
-        else if i >= m0 && j >= m1 then r
-        else loop (i + step_i) j (k.push r v)
+        else if i >= m0 && j < m1 then loop n0 (j + step_j) r
+        else if i >= m0 then r
+        else loop (i + step_i) j (k.push r (f i j))
       in
       k.stop (loop n0 n1 (k.init ()))
     in
